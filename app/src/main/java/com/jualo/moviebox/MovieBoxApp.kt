deeb4e9eb77e5e5f20649ca650f194ce45cb3c68
yogacp <ichi.emo@gmail.com>
2018-05-22 23:00:38 +0700

    initial commit

---
+++ app/src/main/java/com/jualo/moviebox/MovieBoxApp.kt
@@ -0,0 +1,12 @@
+package com.jualo.moviebox
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieBoxApp : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
