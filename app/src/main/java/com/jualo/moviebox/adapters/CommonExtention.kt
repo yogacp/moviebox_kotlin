e75169565395ea78777809d6bcbeb9e6eeb9881e
yogacp <ichi.emo@gmail.com>
2018-05-22 23:15:53 +0700

    Add gradle dependencies and GenericAdapter

---
+++ app/src/main/java/com/jualo/moviebox/adapters/CommonExtention.kt
@@ -0,0 +1,8 @@
+package com.jualo.moviebox.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+infix fun ViewGroup.inflate(layoutResId: Int): View =
+        LayoutInflater.from(context).inflate(layoutResId, this, false)
